416e5544afb359adb36b1cd3bced7ec3c75d4c7f
ManishPrakkash <manishprakkash@gmail.com>
2025-01-15 06:04:54 +0000

    TEST

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ Promise.all([
     faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
     faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
+]).then(startVideo);
 
 
 //func to vid on web
 function startVideo()
 {
-    navigator.getUserMedia(
+    navigator.mediaDevices.getUserMedia(
         {video: {}},
         stream => video.srcObject=stream,
         err=>console.error(err)
     )
 }
 
-video.addEventListener('play')
+video.addEventListener('play',()=>{
+    console.log('sfff')
+});
