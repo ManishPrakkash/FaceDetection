3363ac2cc358e9b3e8e4bc3f8f6de20ac2a27e2d
ManishPrakkash <manishprakkash@gmail.com>
2025-01-15 06:48:41 +0000

    canvas added

--- index.js
+++ index.js
@@ -19,5 +19,15 @@ function startVideo()
 }
 
 video.addEventListener('play',()=>{
-    console.log('sfff')
+
+    const canvas=faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {width:video.width, height:video.height}
+
+    setInterval(async() =>{
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks
+        ().withFaceExpressions()
+        console.log(detections)
+    },100)
 });
